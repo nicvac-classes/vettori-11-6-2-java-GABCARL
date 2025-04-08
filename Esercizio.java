e0ab25bb97ed88bd4bb25f26b922bd0414a4b60a
GABCARL <gabriele.carlone@marconibari.edu.it>
2025-04-08 14:40:22 +0000

    consegnato

--- Esercizio.java
+++ Esercizio.java
@@ -1,29 +1,30 @@
-import java.util.*;
-import java.lang.Math;
-import java.io.FileWriter;
+import java.util.Scanner;
 import java.io.FileReader;
+import java.io.FileWriter;
 
-public class JavaApplication {
+
+class Esercizio {
     private static Scanner input = new Scanner(System.in);
-    private static Scanner reader;
-    private static FileWriter writer;
+    private static Scanner infile;
+    private static FileWriter outfile;
 
-    public static void main(String[] args) {
+    // Il programma parte con una chiamata a main().
+    public static void main(String[] args) throws Exception {
         int c;
         int nv, np, i;
         String e;
-        
+
         // Inizialmente non ci sono libri in biblioteca.
         nv = 0;
         np = 0;
-        
+
         // Alloco più spazio rispetto alla dimensione effettiva perchè sul vettore saranno effettuate operazioni di inserimento e cancellazione. Supponiamo di avere massimo 100 libri in biblioteca.
         String[] v = new String[100];
         String[] p = new String[100];
-        
-        // Inserisco dei libri di partenza
-        nv = leggiFile(v, nv, "libri.txt");
-        np = leggiFile(p, np, "Prestiti.txt");
+
+        // Carico da File i libri presenti in biblioteca e quelli prestati.
+        nv = caricaVettore(v, nv, "Libri.txt");
+        np = caricaVettore(p, np, "Prestiti.txt");
         do {
             c = leggiComando();
             if (c == 1) {
@@ -51,7 +52,7 @@ public class JavaApplication {
                 e = input.nextLine();
                 i = ricercaNelVettore(v, nv, e);
                 if (i >= 0) {
-                    System.out.println("Libro scelto: " + (char) 10 + i + ":" + v[i]);
+                    System.out.println("Libro scelto: " + System.lineSeparator() + i + ":" + v[i]);
                     np = inserisciInVettore(p, np, v[i], 0);
                     nv = eliminaDaVettore(v, nv, i);
                 } else {
@@ -63,25 +64,59 @@ public class JavaApplication {
                     visualizzaVettore(p, np);
                     do {
                         System.out.println("Indice del libro da restituire: ");
-                        i = input.nextInt();
-                    }
-                    while (i >= np);
-                    System.out.println("Libro restituito: " + (char) 10 + i + ":" + p[i]);
+                        i = Integer.parseInt(input.nextLine());
+                    } while (i >= np);
+                    System.out.println("Libro restituito: " + System.lineSeparator() + i + ":" + p[i]);
                     nv = inserisciInVettore(v, nv, p[i], 0);
                     np = eliminaDaVettore(p, np, i);
                 } else {
                     System.out.println("Non ci sono libri in prestito");
                 }
             }
-        }
-        while (c != 6);
-        scriviVettore(v, nv, "libri.txt");
-        scriviVettore(p, np, "Prestiti.txt");
+        } while (c != 6);
+
+        // Salvo i libri in biblioteca ed i prestiti in dei file, in modo da poter ripristinare lo stato dell'applicazione alla prossima apertura del programma.
+        salvaVettore(v, nv, "Libri.txt");
+        salvaVettore(p, np, "Prestiti.txt");
     }
     
+    public static int caricaVettore(String[] v, int nv, String nomeFile) throws Exception {
+        int n, i;
+
+        infile = new Scanner(new FileReader(nomeFile));
+        n = Integer.parseInt(infile.nextLine());
+        i = 0;
+        while (i < n) {
+            v[i] = infile.nextLine();
+            ++i;
+        }
+        infile.close();
+        
+        return n;
+    }
+    
+    public static void salvaVettore(String[] v, int nv, String nomeFile) throws Exception {
+        int i;
+
+        outfile = new FileWriter(nomeFile);
+        //Nel file scrivo i numeri come stringa
+        outfile.write( Integer.toString(nv) );
+        outfile.write(System.lineSeparator()); //Scrivo nel file il ritorno a capo
+
+        i = 0;
+        while (i < nv) {
+            //Nel file scrivo i numeri come stringa
+            outfile.write( v[i] );
+            outfile.write(System.lineSeparator()); //Scrivo nel file il ritorno a capo
+            ++i;
+        }
+        outfile.close();
+    }
+
+
     public static int eliminaDaVettore(String[] v, int n, int ie) {
         int i, n2;
-        
+
         n2 = n - 1;
         i = ie;
         while (i <= n - 2) {
@@ -104,7 +139,7 @@ public class JavaApplication {
     
     public static int inserisciInVettore(String[] v, int n, String e, int ie) {
         int i, n2;
-        
+
         n2 = n + 1;
         i = n2 - 1;
         while (i >= ie + 1) {
@@ -118,40 +153,30 @@ public class JavaApplication {
     
     public static int leggiComando() {
         int c;
-        
+
         do {
-            System.out.println("1) Nuovo libro in biblioteca" + (char) 10 + "2) Visualizza libri in biblioteca" + (char) 10 + "3) Visualizza libri in prestito" + (char) 10 + "4) Prendi in prestito un libro" + (char) 10 + "5) Restituisci un libro" + (char) 10 + "6) Esci dal programma");
-            c = input.nextInt();
-        }
-        while (c < 1 || 6 < c);
+            System.out.println(
+                "1) Nuovo libro in biblioteca" + System.lineSeparator() + 
+                "2) Visualizza libri in biblioteca" + System.lineSeparator() + 
+                "3) Visualizza libri in prestito" + System.lineSeparator() +
+                "4) Prendi in prestito un libro" + System.lineSeparator() +
+                "5) Restituisci un libro" + System.lineSeparator() + 
+                "6) Esci dal programma");
+            c = Integer.parseInt(input.nextLine());
+        } while (c < 1 || 6 < c);
         
         return c;
     }
     
-    public static int leggiFile(String[] v, int nv, String nomefile) {
-        int n, i;
-        
-        reader = new Scanner(new File(nomefile));
-        n = reader.nextInt();
-        i = 0;
-        while (i < n) {
-            v[i] = reader.nextLine();
-            i = i + 1;
-        }
-        reader.close();
-        
-        return n;
-    }
-    
     public static int ricercaNelVettore(String[] v, int n, String valore) {
         int i, iTrovato;
-        
+
         i = 0;
-        iTrovato = (int) (-1);
+        iTrovato = -1;
         while (i < n && iTrovato == -1) {
-            
+
             // Controllo se valore è una sottostringa di V[i]
-            if (sottostringa(valore, v[i]) != -1) {
+            if ( v[i].indexOf(valore) != -1) {
                 iTrovato = i;
             }
             i = i + 1;
@@ -159,73 +184,18 @@ public class JavaApplication {
         
         return iTrovato;
     }
-    
-    public static void scriviVettore(String[] v, int nv, String nomefile) {
-        int i;
         
-        writer = new FileWriter(nomefile);
-        writer.write(Nv + "\n");
-        i = 0;
-        while (i < nv) {
-            writer.write(V[i] + "\n");
-            i = i + 1;
-        }
-        writer.close();
-    }
-    
-    public static int sottostringa(String sottoStr, String stringa) {
-        int i, j, iCurr, iTrovato, lungStr, lungSSt;
-        String primoCarattere;
-        boolean continua;
-        
-        iTrovato = (int) (-1);
-        lungStr = stringa.length();
-        lungSSt = sottoStr.length();
-        primoCarattere = sottoStr.charAt(0);
-        iCurr = 0;
-        while (iCurr < lungStr && iTrovato == -1) {
-            i = iCurr;
-            continua = true;
-            while (i < lungStr && continua) {
-                if (!primoCarattere.equals(stringa.charAt(i))) {
-                    i = i + 1;
-                } else {
-                    continua = false;
-                }
-            }
-            iCurr = i;
-            if (i < lungStr) {
-                iTrovato = iCurr;
-                i = i + 1;
-                j = 1;
-                continua = true;
-                while (i < lungStr && j < lungSSt && continua) {
-                    if (stringa.charAt(i).equals(sottoStr.charAt(j))) {
-                        i = i + 1;
-                        j = j + 1;
-                    } else {
-                        continua = false;
-                    }
-                }
-                if (j < lungSSt) {
-                    iTrovato = (int) (-1);
-                }
-            } else {
-                iTrovato = (int) (-1);
-            }
-            iCurr = iCurr + 1;
-        }
-        
-        return iTrovato;
-    }
-    
     public static void visualizzaVettore(String[] v, int n) {
         int i;
-        
+        System.out.println();
+        System.out.println("-----------------------");
         i = 0;
         while (i < n) {
             System.out.println(Integer.toString(i) + ": " + v[i]);
             i = i + 1;
         }
+        System.out.println("-----------------------");
+        System.out.println();
+
     }
 }
